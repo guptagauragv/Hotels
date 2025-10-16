b208bc1ddf839e63a53a7cb7036d9854f0800b84
Gaurav Gupta <guptagauragv@gmail.com>
2025-10-16 14:14:24 +0530

    Next Version of our hotels

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 // Define MongoDB connection URL
-const mongoURL = "mongodb://localhost:27017/hotels"; // replace database with your database name
-
+//const mongoURL = "mongodb://localhost:27017/hotels"; // replace database with your database name
+const mongoURL =
+  "mongodb+srv://guptagauragv:Qwerty@12345@cluster0.lntqoao.mongodb.net/"; // global host
 // setup mongoDB connection0
 
 mongoose.connect(mongoURL);
